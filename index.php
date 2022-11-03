891eea160767e3e0a1ae627192bbd8a954b9038f
Eve Kang <evekangbest@gmail.com>
2022-11-03 14:32:05 +0900

    Add Title, Description, Search input

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>암환우 보호자 커뮤니티</title>
+    <link rel="stylesheet" type="text/css" href="semantic/semantic.css" />
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <script src="semantic/semantic.js"></script>
+    <style>
+    header {
+        padding: 50px;
+        border-bottom: 1px solid grey;
+        text-align: center;
+    }
+
+    article h3 {
+        text-align: center;
+        padding: 20px;
+    }
+
+    article input {
+        width: 50%;
+        height: 35px;
+        border-radius: 5px;
+    }
+
+    #search {
+        text-align: center;
+    }
+    </style>
+</head>
+
+<body>
+    <header>
+        <h1>암환우 보호자 커뮤니티</h1>
+    </header>
+    <nav></nav>
+    <article>
+        <h3>
+            암환우 보호자들의 자유로운 생각과 의견을 나누는 커뮤니티입니다. <br />자유롭게
+            작성해주세요.
+        </h3>
+        <div id="search">
+            <input type="text" placeholder="검색어를 입력하세요" />
+            <button class="ui button">검색</button>
+        </div>
+    </article>
+
+</body>
+
+</html>
